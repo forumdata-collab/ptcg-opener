2f3c0cda258d6b8a63215f2467b22f06b4393903
Hermes <hermes@forumdata-collab.github.io>
2026-08-13 22:21:13 +0800

    feat: 機械超夢換新圖(銀甲版) + 向左移一個身位 + 題庫重編小一至小三奧數60題

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 寶可夢 TCG 抽卡機 — Service Worker 離線快取 */
-const CACHE = 'ptcg-opener-v11';
+const CACHE = 'ptcg-opener-v12';
 const IMG_CACHE = 'ptcg-imgs-v1';
 const CORE = ['./', './index.html', './manifest.json', './icon.svg'];
 
